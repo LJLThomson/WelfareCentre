59987a65e020341389a65e4d6e2a903b3eb5b10d
Li Jia Liang <1114625580@qq.com>
2017-01-17 20:49:41 +0800

    21、用户头像下载，

--- app/src/main/java/cn/ucai/welfarecentre/controller/fragment/PersonalFragment.java
+++ app/src/main/java/cn/ucai/welfarecentre/controller/fragment/PersonalFragment.java
@@ -15,7 +15,10 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.welfarecentre.Model.bean.User;
+import cn.ucai.welfarecentre.Model.utils.ImageLoader;
 import cn.ucai.welfarecentre.R;
+import cn.ucai.welfarecentre.application.FuLiCentreApplication;
 import cn.ucai.welfarecentre.view.MFGT;
 
 /**
@@ -64,16 +67,24 @@ public class PersonalFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_personal, container, false);
         ButterKnife.bind(this, view);
+//        用户头像http://101.251.196.90:8000/FuLiCenterServerV2.0/downloadAvatar?name_or_hxid=12&avatarType=user_avatar&m_avatar_suffix=.jpg&width=60&height=60
+        // downloadAvatar();
         return view;
     }
 
-    @OnClick({R.id.iv_persona_center_msg, R.id.center_user_collects})
+    @OnClick({R.id.iv_persona_center_msg, R.id.center_user_collects,R.id.iv_user_avatar})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_persona_center_msg:
                 MFGT.gotoSettingsActivity(getActivity());
                 break;
             case R.id.center_user_collects:
+                break;
+            case R.id.iv_user_avatar:
+//                图片下载
+                User user = FuLiCentreApplication.getUser();
+                String avatar_img = ImageLoader.getAvatarUrl(user);
+                ImageLoader.setAvatar(avatar_img,getActivity(),ivUserAvatar);
                 break;
         }
     }
