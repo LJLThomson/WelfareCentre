a3046c751c5206e74045a60a89cdf43a3e82b625
Li Jia Liang <1114625580@qq.com>
2017-01-19 20:45:39 +0800

    26、addCart，添加到购物车中

--- app/src/main/java/cn/ucai/welfarecentre/controller/fragment/GoodsDetailsFragment.java
+++ app/src/main/java/cn/ucai/welfarecentre/controller/fragment/GoodsDetailsFragment.java
@@ -1,11 +1,10 @@
 package cn.ucai.welfarecentre.controller.fragment;
 
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,13 +20,11 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.welfarecentre.Model.bean.GoodsDetailsBean;
 import cn.ucai.welfarecentre.Model.bean.MessageBean;
-import cn.ucai.welfarecentre.Model.bean.Result;
 import cn.ucai.welfarecentre.Model.bean.User;
 import cn.ucai.welfarecentre.Model.net.ModelNewGoods;
 import cn.ucai.welfarecentre.Model.net.OnCompleteListener;
 import cn.ucai.welfarecentre.Model.utils.CommonUtils;
 import cn.ucai.welfarecentre.Model.utils.I;
-import cn.ucai.welfarecentre.Model.utils.ImageLoader;
 import cn.ucai.welfarecentre.R;
 import cn.ucai.welfarecentre.application.FuLiCentreApplication;
 import cn.ucai.welfarecentre.view.FlowIndicator;
@@ -66,7 +63,9 @@ public class GoodsDetailsFragment extends Fragment {
     int good_id;
     boolean isCollect = false;
     int Collectcount;
-
+    @BindView(R.id.imgCart)
+    ImageView imgCart;
+    boolean isCart = false;
     public GoodsDetailsFragment() {
         // Required empty public constructor
     }
@@ -134,6 +133,36 @@ public class GoodsDetailsFragment extends Fragment {
             MFGT.gotoLoginActivity(getActivity());
         }
     }
+    @OnClick(R.id.imgCart)
+    public void addCart(){//购物车
+        User user = FuLiCentreApplication.getUser();
+        if (user!=null){
+            model.addCart(getActivity(), good_id, user.getMuserName(), 1, new OnCompleteListener<MessageBean>() {
+                @Override
+                public void onSuccess(MessageBean result) {
+                    if (result!=null && result.isSuccess()){//添加成功
+                        isCart = true;
+                    }else{
+                        isCart = false;
+                    }
+                    setCartStatus();
+                }
+
+                @Override
+                public void onError(String error) {
+
+                }
+            });
+        }
+    }
+
+    private void setCartStatus() {
+        if (isCart){
+          imgCart.setBackgroundColor(Color.RED);//被点击时，设置为红色，确定为被放入购物车
+        }else{
+            imgCart.setImageResource(R.mipmap.bg_cart_selected);
+        }
+    }
 
     private void getCollectCount(User user) {
         model.getCollectCount(getActivity(), user.getMuserName(), new OnCompleteListener<MessageBean>() {
@@ -141,8 +170,8 @@ public class GoodsDetailsFragment extends Fragment {
             public void onSuccess(MessageBean result) {
                 if (result != null && result.isSuccess()) {//计数成功
                     Collectcount = Integer.parseInt(result.getMsg());
-//             发送广播       getActivity().sendBroadcast();
-                    MFGT.sendtoPersonalActivity(getActivity(), Collectcount);
+//             发送广播getActivity().sendBroadcast();
+                    MFGT.sendtoPersonalActivity(getActivity());
                 }
             }
 
@@ -157,7 +186,7 @@ public class GoodsDetailsFragment extends Fragment {
     public void onResume() {
         super.onResume();
         User user = FuLiCentreApplication.getUser();
-        initCollectStatus(user);//用于判断是否被收藏，收藏之后，显示红色
+        initCollectStatus(user);//用于判断是否被收藏，收藏之后，显示红色,这是从数据库中得来的，这边不会有影响
     }
 
     private void initCollectStatus(User user) {
