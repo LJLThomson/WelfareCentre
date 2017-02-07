85bb077a9adc05a86803fc2af4849e85b0243b49
Li Jia Liang <1114625580@qq.com>
2017-02-07 12:24:27 +0800

    25、收藏查看，以及删除收藏，并及时刷新

--- app/src/main/java/cn/ucai/welfarecentre/controller/activity/OrderActivity.java
+++ app/src/main/java/cn/ucai/welfarecentre/controller/activity/OrderActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.welfarecentre.controller.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -10,6 +11,17 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.pingplusplus.android.PingppLog;
+import com.pingplusplus.libone.PaymentHandler;
+import com.pingplusplus.libone.PingppOne;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,6 +36,8 @@ import cn.ucai.welfarecentre.R;
  */
 
 public class OrderActivity extends AppCompatActivity {
+    private static String URL = "http://218.244.151.190/demo/charge";
+
     @BindView(R.id.backClickArea)
     LinearLayout backClickArea;
     @BindView(R.id.etReceiverName)
@@ -48,6 +62,17 @@ public class OrderActivity extends AppCompatActivity {
         int sumPrice = getIntent().getIntExtra(I.Cart.SAVAPRICE, 0);
         tvCount.setText("" + sumPrice);
         readloginfo();
+        initPingPP();
+    }
+
+    private void initPingPP() {
+        // 设置要使用的支付方式
+        PingppOne.enableChannels(new String[]{"wx", "alipay", "upacp", "bfb"});
+        //提交数据的格式，默认格式为json
+        //PingppOne.CONTENT_TYPE = "application/x-www-form-urlencoded";
+        PingppOne.CONTENT_TYPE = "application/json";
+        //是否开启日志
+        PingppLog.DEBUG = true;
     }
 
     private void readloginfo() {
@@ -87,7 +112,7 @@ public class OrderActivity extends AppCompatActivity {
             etPhone.requestFocus();
             return;
         }
-        final int[] cityPosition = new int[1];
+        final int[] cityPosition = new int[]{0};
         spinArea.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
@@ -109,6 +134,50 @@ public class OrderActivity extends AppCompatActivity {
         }
         SharePrefrenceUtils utils = SharePrefrenceUtils.getInstance(this);
         utils.savaAddress(CusumerName, phoneNumber, cityPosition[0], Address);
-        
+        showPay();
+    }
+
+    private void showPay() {
+        // 产生个订单号
+        String orderNo = new SimpleDateFormat("yyyyMMddhhmmss")
+                .format(new Date());
+
+        // 计算总金额（以分为单位）
+        int amount = 0;
+        JSONArray billList = new JSONArray();
+   /*     for (Good good : mList) {
+            amount += good.getPrice() * good.getCount() * 100;
+            billList.put(good.getName() + " x " + good.getCount());
+        }*/
+        // 构建账单json对象
+        JSONObject bill = new JSONObject();
+
+        // 自定义的额外信息 选填
+        JSONObject extras = new JSONObject();
+        try {
+            extras.put("extra1", "extra1");
+            extras.put("extra2", "extra2");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            bill.put("order_no", orderNo);
+            bill.put("amount", amount);
+            bill.put("extras", extras);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //壹收款: 创建支付通道
+        PingppOne.showPaymentChannels(getSupportFragmentManager(), bill.toString(), null, URL, new PaymentHandler() {
+
+            // 返回支付结果
+            // @param data
+
+            @Override
+            public void handlePaymentResult(Intent data) {
+            }
+        });
     }
 }
